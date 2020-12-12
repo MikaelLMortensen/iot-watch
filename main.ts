becef9db461bb9564121e802f0dd0b8b82f72e8c
MikaelLMortensen <MikaelLMortensen@users.noreply.github.com>
2020-12-12 07:29:08 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     let setHour = RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR)
     setHour = setHour + 1
-    if (setHour > 24) {
+    if (setHour >= 24) {
         setHour = 0
     }
     RTC_DS1307.setTime(RTC_DS1307.TimeType.HOUR, setHour)
-})
-
-input.onButtonPressed(Button.AB, function () {
-    RTC_DS1307.DateTime(2020, 12, 08, 14, 28, 0)
 })
 
 input.onButtonPressed(Button.B, function () {
@@ -20,39 +16,45 @@ input.onButtonPressed(Button.B, function () {
     RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, setMinute)
 })
 
-basic.forever(function () {
-    let hour = RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR)
-    let minute = RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE)
-    let second = RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND)
-
+input.onButtonPressed(Button.AB, function () {
     let hourString = "";
     if (hour < 10){
         hourString = "0"
     }
     hourString = hourString + hour.toString()
-    let hourValue = Math.floor((1023 / 23) * hour)
-    pins.analogWritePin(AnalogPin.P0, hourValue)
 
     let minuteString = "";
     if (minute < 10){
         minuteString = "0"
     }
     minuteString = minuteString + minute.toString()
-    let minuteValue = Math.floor((1023 / 60) * minute)
-    pins.analogWritePin(AnalogPin.P1, minuteValue)
 
     let secondString = "";
     if (second < 10){
         secondString = "0"
     }
     secondString = secondString + second.toString()
-    let secondValue = Math.floor((1023 / 60) * second)
-    pins.analogWritePin(AnalogPin.P2, secondValue)
 
     basic.showString(hourString + ":" + minuteString + "." + secondString +  "          ")
 })
 
+
+basic.forever(function () {
+    hour = RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR)
+    minute = RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE)
+    second = RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND)
+
+    let hourValue = Math.floor(1023 * hour / 24)
+    let minuteValue = Math.floor(1023 * minute / 60)
+    let secondValue = Math.floor(1023 * second / 60)
+
+    pins.analogWritePin(AnalogPin.P0, secondValue)
+    pins.analogWritePin(AnalogPin.P1, minuteValue)
+    pins.analogWritePin(AnalogPin.P2, hourValue)
+})
+
+let hour = 0
+let minute = 0
+let second = 0
 RTC_DS1307.DateTime(2020, 12, 08, 14, 28, 0)
-
-
 
